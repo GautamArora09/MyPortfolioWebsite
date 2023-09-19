7ea59d676e2dc54bf3ee178113527d55b21d7636
GautamArora09 <gautamaroragithub@gmail.com>
2023-09-19 11:05:29 +0530

    Adding some extra features

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -53,14 +53,14 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              {/* <div className="flex items-center justify-center">
+              <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
                 </button>
-              </div> */}
+              </div>
             </div>
           ))}
         </div>
